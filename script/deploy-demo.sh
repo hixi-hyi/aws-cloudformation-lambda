502d70fb2d07be65cc9ca202884119801db8b92d
Hiroyoshi HOUCHI <git@hixi-hyi.com>
2019-04-20 18:33:08 +0900

    Feature/cloudformation stack (#6)
    * feature/cloudformation-stack
    
    * add Returnvalue to README

--- script/deploy-demo.sh
+++ script/deploy-demo.sh
@@ -4,5 +4,9 @@ if [ -z "$1" ]; then
   exit 1
 fi
 function=$1
+if [ "$function" == "cloudformation-stack" ]; then
+    bucket=$(aws cloudformation list-exports --query "Exports[?Name==\`cfn-lambda-s3:BucketName\`].Value" --output "text")
+    aws s3api put-object --bucket ${bucket} --key demo/cloudformation-stack/resource.yaml --body function/cloudformation-stack/tests/resource.yaml > /dev/null
+fi
 date=$(date)
 aws cloudformation deploy --stack-name cfn-lambda-${function}-demo --template-file demo/${function}/demo.yaml --parameter-overrides Date="$date"
